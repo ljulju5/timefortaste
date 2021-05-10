bdeb53c0547f171f90a3f39f95b0158ea5b0ae05
Ljubica Carkic <carkic5@gmail.com>
2021-05-10 07:48:48 +0200

    responsive design

--- js/main.js
+++ js/main.js
@@ -24,3 +24,24 @@ btnHamburger.addEventListener('click', function(){
 
   }  
 });
+
+// Back to top button
+//Get the button
+var topbutton = document.getElementById("goToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    topbutton.style.display = "block";
+  } else {
+    topbutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
